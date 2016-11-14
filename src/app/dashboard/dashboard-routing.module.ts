678296321dffeae93adeabd1a071db4c8aee441a
Antony <antony.budianto@gdplabs.id>
2016-11-14 10:00:33 +0700

    reproduce loadconfig bug

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -2,14 +2,23 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { DashboardComponent } from './dashboard.component';
+import { HomeComponent } from '../home/home.component';
 
 const routes: Routes = [
   {
     path: '',
     component: DashboardComponent,
     children: [
+      /**
+       * Uncomment this, then open console
+       * see the 2nd object, `_loadedConfig` is not there
+       **/
       {
         path: '',
+        component: HomeComponent
+      },
+      {
+        path: 'feat1',
         loadChildren: './feat1/feat1.module#Feat1Module'
       },
       {
--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
 import { DashboardRoutingModule } from './dashboard-routing.module';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   imports: [
     CommonModule,
     DashboardRoutingModule
   ],
-  declarations: [DashboardComponent]
+  declarations: [DashboardComponent, MainComponent]
 })
 export class DashboardModule { }
